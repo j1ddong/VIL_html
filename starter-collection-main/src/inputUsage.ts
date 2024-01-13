4dc896f3d111978ef8bb2768a704c1a2c6b566e2
j1ddong <hju191228@gmail.com>
2024-01-13 10:47:36 -0800

    puzzle_game_input_control

--- starter-collection-main/src/inputUsage.ts
+++ starter-collection-main/src/inputUsage.ts
@@ -1,7 +1,10 @@
 import { waitForKeyPress, waitForUserInput } from "./utils/input/input.utils";
 
 async function main() {
-  const answer = await waitForUserInput("SML 중에 선택해주세요");
-  console.log(answer);
+  let condition: boolean = false;
+  do {
+    const answer = await waitForUserInput("SML 중에 선택해주세요");
+    condition = answer === "S" || answer === "M" || answer === "L";
+  } while (!condition);
 }
 main();
