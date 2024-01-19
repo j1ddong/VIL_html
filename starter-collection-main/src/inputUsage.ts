df2648e04c6b24c927fc5aa13f6255e946c00440
j1ddong <hju191228@gmail.com>
2024-01-19 10:53:58 -0800

    optimazation key control

--- starter-collection-main/src/inputUsage.ts
+++ starter-collection-main/src/inputUsage.ts
@@ -1,115 +1,151 @@
 import { waitForKeyPress, waitForUserInput } from "./utils/input/input.utils";
 
+let lst: (number | string)[][] = [];
+let lstLen: number = 0;
+let row: number = 0;
+let column: number = 0;
+
 const getRandomInt = (max: number): number => {
-  return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max) + 1;
 };
 
-const getRandomIntLst = (n: number): number[][] => {
-  const lst: number[][] = [];
-  for (let i = 0; i < n; i++) {
-    let tempLst: number[] = [];
-    for (let j = 0; j < n; j++) {
-      const randomNum = getRandomInt(9);
-      tempLst.push(randomNum);
+/**
+ * 1  2  3
+ * 4  5  6
+ * 8  7
+ */
+
+const getRandomIntLst = () => {
+  const uniqueNums = new Set();
+  const maxNum = lstLen * lstLen;
+  for (let i = 0; i < lstLen; i++) {
+    let tempLst: (number | string)[] = [];
+    for (let j = 0; j < lstLen; j++) {
+      let randomNum: number = getRandomInt(maxNum);
+
+      while (uniqueNums.has(randomNum)) {
+        randomNum = getRandomInt(maxNum);
+      }
+
+      if (randomNum === maxNum) {
+        tempLst.push(" ");
+        row = i;
+        column = j;
+      } else {
+        tempLst.push(randomNum);
+      }
+      uniqueNums.add(randomNum);
     }
     lst.push(tempLst);
   }
-  return lst;
 };
 
-const logLst = (lst: number[][], n: number, i: number, j: number, targetNum: number): number => {
-  let lstSum: number = 0;
+const logLst = (): void => {
   console.clear();
-  console.log(`합이 ${targetNum}이 되도록 숫자를 바꿔보세요` + "\n");
-  for (let i = 0; i < n; i++) {
-    process.stdout.write("| ");
-    for (let j = 0; j < n; j++) {
-      process.stdout.write(lst[i][j] + " ");
-      lstSum += lst[i][j];
+  for (let i = 0; i < lstLen; i++) {
+    let tempString = "| ";
+    for (let j = 0; j < lstLen; j++) {
+      tempString += lst[i][j] + " ";
     }
-    process.stdout.write("|" + "\n");
+    tempString += "|";
+    console.log(tempString);
   }
   console.log();
-  console.log("현재합:", lstSum);
-  console.log("현재 선택된 값:", lst[i][j]);
-  return lstSum;
 };
 
 const convertLen = (inputString: string): number => {
-  let n: number = 0;
   switch (inputString) {
     case "S":
-      n = 3;
-      break;
+      return 3;
     case "M":
-      n = 4;
-      break;
+      return 4;
     case "L":
-      n = 5;
-      break;
+      return 5;
   }
-  return n;
+  return 0;
 };
 
-const keyCtrl = (keyInputCtrl: boolean, keyInputName: string, n: number, i: number, j: number) => {
-  if (keyInputCtrl && keyInputName === "c") {
-    return false;
-  } else if (!keyInputCtrl) {
-    if (!isNaN(parseInt(keyInputName))) {
-      return [i, j, parseInt(keyInputName)];
-    } else {
-      switch (keyInputName) {
-        case "up":
-          i === 0 ? (i = n - 1) : i--;
-          break;
-        case "down":
-          i === n - 1 ? (i = 0) : i++;
-          break;
-        case "left":
-          j === 0 ? (j = n - 1) : j--;
-          break;
-        case "right":
-          j === n - 1 ? (j = 0) : j++;
-          break;
-      }
-      return [i, j, -1];
+const keyCtrl = (keyInputCtrl: boolean, keyInputName: string): void => {
+  if (!keyInputCtrl) {
+    switch (keyInputName) {
+      case "up":
+        if (row !== 0) {
+          const originalVal = lst[row][column];
+          lst[row][column] = lst[row - 1][column];
+          lst[row - 1][column] = originalVal;
+          row--;
+        }
+        break;
+      case "down":
+        if (row !== lstLen - 1) {
+          const originalVal = lst[row][column];
+          lst[row][column] = lst[row + 1][column];
+          lst[row + 1][column] = originalVal;
+          row++;
+        }
+        break;
+      case "left":
+        if (column !== 0) {
+          const originalVal = lst[row][column];
+          lst[row][column] = lst[row][column - 1];
+          lst[row][column - 1] = originalVal;
+          column--;
+        }
+        break;
+      case "right":
+        if (column !== lstLen - 1) {
+          const originalVal = lst[row][column];
+          lst[row][column] = lst[row][column + 1];
+          lst[row][column + 1] = originalVal;
+          column++;
+        }
+        break;
     }
   }
 };
+
+const isSolved = (): boolean => {
+  for (let row = 0; row < lstLen; row++) {
+    for (let col = 0; col < lstLen; col++) {
+      if (row === lstLen - 1 && col === lstLen - 1 && lst[row][col] === " ") {
+        return true;
+      } else if (lst[row][col] !== row * lstLen + col + 1) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// 파일 나누기 > 최소 4개 [main, init, ctrl, console] variable export import
+// random lst 만들기...
+// count display
+// time remaining > setInterval
 
 async function main() {
   let condition: boolean = false;
-
+  let inputString: string = "";
   do {
-    let inputString: string = await waitForUserInput("SML 중에 선택해주세요");
+    inputString = await waitForUserInput("SML 중에 선택해주세요");
     inputString = inputString.toLocaleUpperCase();
     condition = inputString === "S" || inputString === "M" || inputString === "L";
-
-    if (condition) {
-      let n: number = convertLen(inputString);
-      const targetNum: number = getRandomInt(9 * n);
-      const lst: number[][] = getRandomIntLst(n);
-      let lstSum: number = 0;
-      let i: number = 0;
-      let j: number = 0;
-      lstSum = logLst(lst, n, i, j, targetNum);
-
-      while (lstSum !== targetNum) {
-        let keyInput = await waitForKeyPress();
-        const keyResult = keyCtrl(keyInput.ctrl, keyInput.name, n, i, j);
-        if (keyResult) {
-          i = keyResult[0];
-          j = keyResult[1];
-          let newNumber = keyResult[2];
-          if (newNumber !== -1) {
-            lst[i][j] = newNumber;
-          }
-          lstSum = logLst(lst, n, i, j, targetNum);
-        } else {
-          break;
-        }
-      }
-    }
   } while (!condition);
+
+  lstLen = convertLen(inputString);
+  getRandomIntLst();
+  logLst();
+
+  while (!isSolved()) {
+    let keyInput = await waitForKeyPress();
+    let keyInputCtrl: boolean = keyInput.ctrl;
+    let keyInputName: string = keyInput.name;
+
+    if (keyInputCtrl && keyInputName === "c") {
+      return;
+    }
+    keyCtrl(keyInput.ctrl, keyInput.name);
+    logLst();
+  }
+  console.log("게임해결!");
 }
 main();
